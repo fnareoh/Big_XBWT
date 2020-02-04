2c20803bcc2afdda3a4918fe7779ba7aab3aef4d
fnareoh <garance.gourdel@gmail.com>
2020-02-04 19:13:08 +0100

    Add documentation for kmacs

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,10 @@
+# usage: ./run_test.sh at the root of the project
+# it computes the bwt of the ref and reads in REF and READS
+# according to a naive implementation and the prefix free parsing
+# There can be differences (they are ouputed by test/compare.py)
+# when the context is precisely the same, because the order then
+# depends on the parsing which the naive implementation doesn't have.
+# But there should be relativly few (0 for small, 2 for medium, 8 for ref.in).
 REF=(data/small_ref.in data/medium_ref.in data/ref.in)
 READS=(data/small_reads.in data/medium_reads.in data/reads.in)
 
